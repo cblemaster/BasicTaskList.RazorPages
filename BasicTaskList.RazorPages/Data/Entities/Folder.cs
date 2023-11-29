5503e8f3671e21beb6f06c5fee90f4cdd4d7f17d
Brian LeMaster <brian.lemaster@outlook.com>
2023-11-29 11:33:37 -0500

    added data annotations to entity classes, and added helper properties to folder entity

--- BasicTaskList.RazorPages/Data/Entities/Folder.cs
+++ BasicTaskList.RazorPages/Data/Entities/Folder.cs
@@ -1,10 +1,21 @@
-﻿namespace BasicTaskList.RazorPages.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BasicTaskList.RazorPages.Data.Entities;
 
 public partial class Folder
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(255)]
     public string Name { get; set; } = null!;
 
     public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+
+    [NotMapped]
+    public bool HasTasks => Tasks.Any();
+
+    [NotMapped]
+    public int TasksCount => HasTasks ? Tasks.Count : 0;
 }
--- BasicTaskList.RazorPages/Data/Entities/Task.cs
+++ BasicTaskList.RazorPages/Data/Entities/Task.cs
@@ -1,17 +1,26 @@
-﻿namespace BasicTaskList.RazorPages.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicTaskList.RazorPages.Data.Entities;
 
 public partial class Task
 {
     public int Id { get; set; }
 
+    [Required]      //TODO: Can data annotations be replaced with fluent api in the db context?
+    [MaxLength(255)]
     public string Name { get; set; } = null!;
 
+    [MaxLength(255)]
     public string? Notes { get; set; }
 
+    [Display(Name = "Due Date")]
+    [DisplayFormat(DataFormatString = "{0:d}")]
     public DateTime DueDate { get; set; }
 
+    [Display(Name = "Is Complete")]
     public bool IsComplete { get; set; }
 
+    [Display(Name = "Is Important")]
     public bool IsImportant { get; set; }
 
     public int FolderId { get; set; }
