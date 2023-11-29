33ebb826239a72565b5ea371d5d8e42e2ef473d1
Brian LeMaster <brian.lemaster@outlook.com>
2023-11-29 16:28:06 -0500

    debugged tasks nav changes

--- BasicTaskList.RazorPages/Pages/Tasks/Index.cshtml.cs
+++ BasicTaskList.RazorPages/Pages/Tasks/Index.cshtml.cs
@@ -13,15 +13,19 @@ namespace BasicTaskList.RazorPages.Pages.Tasks
 
         public IList<Task> Task { get; set; } = default!;
 
+        public int? FolderId { get; set; }
+
         public bool IsShowingAllTasks { get; set; }
 
-        public async System.Threading.Tasks.Task OnGetAsync(int? id)
+        public async System.Threading.Tasks.Task OnGetAsync(int? folderid)
         {
+            FolderId = folderid;
+            
             if (_context.Tasks != null)
             {
-                if (id != null)
+                if (folderid != null)
                 {
-                    Task = await _context.Tasks.Where(t => t.FolderId == id)
+                    Task = await _context.Tasks.Where(t => t.FolderId == folderid)
                         .Include(t => t.Folder).ToListAsync();
                 }
                 else
